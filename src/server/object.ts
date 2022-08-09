d9009b00bc24aba3432881568418c9cb2dd86f14
failip <philipp@kuboth.dev>
2022-08-09 20:47:04 +0200

    Move to translation for position

--- src/server/object.ts
+++ src/server/object.ts
@@ -1,12 +1,14 @@
 import { vec3, mat4, quat } from 'gl-matrix';
 
 export class Object {
-    position: vec3;
+    translation: vec3;
+    rotation: vec3;
     id: string;
 
     constructor(name: string) {
         this.id = name;
-        this.position = vec3.create();
+        this.translation = vec3.create();
+        this.rotation = vec3.create();
     }
 
     getJSONRepresentation() {
--- src/server/room.ts
+++ src/server/room.ts
@@ -3,25 +3,26 @@ import { Object } from './object';
 
 export class Room {
     clients: Client[];
-    objects: {};
+    objects: Map<string, Object>;
 
     constructor() {
         this.clients = [];
-        this.objects = {};
+        this.objects = new Map();
         let cube = new Object('Cube');
         this.objects['Cube'] = cube;
         console.log(cube.getJSONRepresentation());
     }
 
     addClient(client: Client) {
+        console.log(client);
         this.clients.push(client);
         client.sendMessage(JSON.stringify(this.objects));
     }
 
     updateRoom(update) {
-        this.objects['Cube'].position[0] = update.x;
-        this.objects['Cube'].position[1] = update.y;
-        this.objects['Cube'].position[2] = update.z;
+        this.objects['Cube'].translation[0] = update.x;
+        this.objects['Cube'].translation[1] = update.y;
+        this.objects['Cube'].translation[2] = update.z;
         for (const client_index in this.clients) {
             this.clients[client_index].sendMessage(JSON.stringify(this.objects));
         }
